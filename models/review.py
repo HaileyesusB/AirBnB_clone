09526f8af56d97fe2585c95bb0a86d7d97760de5
Haileyesus Belew <88820045+HaileyesusB@users.noreply.github.com>
2022-10-30 23:48:15 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Review Class"""
+"""
+This module defines a class called Review that inherits
+from the BaseModel class
+"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class that inherit from BaseModel"""
+    """
+    This class represents a Review object and inherits from
+    a BaseModel class.
+    """
     place_id = ""
     user_id = ""
     text = ""
